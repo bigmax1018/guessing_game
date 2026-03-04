64b52fedac0ebfe8974bc781f3da531b46832686
BMAX <152480620+bigmax1018@users.noreply.github.com>
2026-03-04 09:35:01 -0500

    update code

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ use std::cmp::Ordering;
 use rand::Rng;
 fn main() {
     println!("Guess the number!");
-
     let secret_number = rand::thread_rng().gen_range(1..101);
-
     loop {
         println!("Please input your guess.");
 
